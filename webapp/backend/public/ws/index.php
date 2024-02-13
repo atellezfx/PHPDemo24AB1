58d5c593c1eb967c01027bb88e0ab0d9d48e0bde
atellezf <alejandro@correo.com>
2024-02-12 21:15:39 -0600

    Código de OpenClass S7

--- webapp/backend/public/ws/index.php
+++ webapp/backend/public/ws/index.php
@@ -3,13 +3,18 @@
 use DogtorPET\Backend\Rest\LoginController;
 use DogtorPET\Backend\Rest\TipoController;
 use DogtorPET\Backend\Rest\MascotaController;
+use DogtorPET\Backend\Util\Config;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Tuupola\Middleware\CorsMiddleware;
+use Tuupola\Middleware\JwtAuthentication;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
 $app = AppFactory::create();
+$config = Config::obtenerInstancia();
+$logger = $config->crearLog();
 
 $app->get('/ws/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
@@ -17,19 +22,39 @@ $app->get('/ws/hello/{name}', function (Request $request, Response $response, ar
     return $response;
 });
 
-// TODO: Configurar intermediarios (middleware) para habilitar las llamadas remotas (CORS) en fase de desarrollo
+### ENSAMBLANDO LOS INTERMEDIARIOS (MIDDLEWARE) ###############################################
+$app->add( new CorsMiddleware([
+    'origin'=>['*'], // IMPORTANTE!!: Sólo en fase de desarrollo (no usar en producción)
+    'methods' => ['GET','POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    'headers.allow' => ['Authorization', 'Content-Type', '*'],
+    'logger' => $logger
+]) );
 
-### DESPACHANDO LLAMADAS CON EL CONTROLADOR DE LOGIN
+$app->add( new JwtAuthentication([
+    'path' => ['/ws'],
+    'ignore' => ['/ws/login'],
+    'secret' => $config->jwtSecret(),
+    'logger' => $logger,
+    'secure' => false, // IMPORTANTE!!: Permite llamadas por HTTP (no usar en producción)
+    'error' => function($res, $args) {
+        $mensaje = ['codigo'=>'error', 'mensaje'=>$args['message']];
+        $res->getBody()->write( json_encode( $mensaje ) );
+        return $res->withHeader( 'Content-Type', 'application/json' )->withStatus(401);
+    }
+]) );
+
+### DESPACHANDO LLAMADAS CON EL CONTROLADOR DE LOGIN ##########################################
 $app->post('/ws/login', LoginController::class . ':login');
 
-### DESPACHANDO LLAMADAS CON EL CONTROLADOR DE TIPO
+### DESPACHANDO LLAMADAS CON EL CONTROLADOR DE TIPO ###########################################
 $app->get('/ws/tipos', TipoController::class .':lista');
 
-### DESPACHANDO LLAMADAS CON EL CONTROLADOR DE MASCOTA
+### DESPACHANDO LLAMADAS CON EL CONTROLADOR DE MASCOTA ########################################
 $app->get('/ws/mascota/{id}', MascotaController::class .':obtener');
-$app->get('/ws/mascotas/{propietario}', MascotaController::class .':lista');
-
-// TODO: Implementar las urls para el webservice de insertar, actualizar y eliminar mascotas
+$app->get('/ws/mascota/catalogo/{propietario}', MascotaController::class .':lista');
+$app->post('/ws/mascota', MascotaController::class . ':insertar');
+$app->put('/ws/mascota/{id}', MascotaController::class .':actualizar');
+$app->delete('/ws/mascota/{id}', MascotaController::class .':eliminar');
 
 $app->run();
 ?>
--- webapp/dogtorpet/src/app/components/login/login.component.ts
+++ webapp/dogtorpet/src/app/components/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginService } from '../../services/login.service';
+import { AUTH_TOKEN, LoginService, USUARIO_ACTUAL } from '../../services/login.service';
 import { Router } from '@angular/router';
 import { Usuario } from '../../models/usuario';
+import { Token } from '../../models/token';
+import { Mensaje } from '../../models/mensaje';
 
 @Component({
   selector: 'app-login',
@@ -25,17 +27,22 @@ export class LoginComponent {
 
   public enviarDatos() {
     const credenciales = this.formulario.value;
-    // console.log( credenciales );
     this.loginSvc.login( credenciales ).subscribe( {
       next: datos => this.procesarRespuesta( datos, credenciales ),
       error: datos => this.mensajeError = datos
     } );
   }
 
-  public procesarRespuesta(datos:any, usuario:Usuario): void {
-    this.router.navigateByUrl('/catalogo');
+  public procesarRespuesta(datos:Token|Mensaje, usuario:Usuario): void {
+    if('token' in datos) {
+      localStorage.setItem( USUARIO_ACTUAL, usuario.username );
+      localStorage.setItem( AUTH_TOKEN, datos.token );
+      this.router.navigateByUrl('/catalogo');
+    } else {
+      localStorage.removeItem( USUARIO_ACTUAL );
+      localStorage.removeItem( AUTH_TOKEN );
+      this.mensajeError = `${datos.codigo}: ${datos.mensaje}`;
+    }
   }
-
-
 
 }
